0962cdb878b16682923d2656e4c14cc734d03e43
Yu Lu <tcsxxs@gmail.com>
2020-08-20 23:33:34 +0800

    Scipy_optimize_chi2 used
    Motivated by https://stackoverflow.com/questions/22177576/python-minimizing-chi-squared.
    I used scipy.optimize to minimize a self-defined chi2

---
+++ scipy_optimize_chi2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from scipy import optimize
+norm = pd.read_excel (r'C:\Users\lenovo\OneDrive\桌面\Physics\Pioneer Academics\Independent Project\Data\norm.xlsx')
+def chisq(c):
