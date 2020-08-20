473bd18599c09aa4e4d6b7ad589ed06dcaa2896c
Yu Lu <tcsxxs@gmail.com>
2020-08-20 20:39:22 +0800

    Get the scipy method work
    develop the scipy method so that it works to generate a result.

--- scipy_linear_regression.py
+++ scipy_linear_regression.py
@@ -7,7 +7,9 @@ norm = pd.read_excel (r'C:\Users\lenovo\OneDrive\桌面\Physics\Pioneer Academic
 #introduce the data matrix and the result vector
 #subject to lb <= x <= ub
 A=norm[['s2_norm_mu','s3_norm_mu','s4_norm_mu','s5_norm_mu']].to_numpy()
-b=norm[['s1_norm_mu']].to_numpy()
-lb=0.0
-ub=1.0
-res = lsq_linear(A, b, bounds=(lb, ub), lsmr_tol='auto', verbose=1)
+print(A)
+b=norm['s1_norm_mu'].to_numpy()
+print(b)
+lb=np.array([0.0,0.0,0.0,0.0],np.float64)
+ub=np.array([1.0,1.0,1.0,1.0],np.float64)
+res = lsq_linear(A, b, bounds=(lb, ub))
