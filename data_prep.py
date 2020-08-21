eea42a02ce418f77108bf47f2267967247a1b3fd
Yu Lu <tcsxxs@gmail.com>
2020-08-21 21:00:21 +0800

    Combine background subs and normalization
    I first functinoalize the methods I used in background substraction and normalization. Then, these two files are combined so that no intermediate files are needed, which helps the incorporation of new known structures.

--- data_prep.py
+++ data_prep.py
@@ -21,7 +21,7 @@ def back_subs(df, end, num):
          after doing background substraction
     end : int
         it records the index of the last element in pre-tail energy in df
-    num : TYPE
+    num : int
         it records how much elements are considered as pre-tail energy
 
     Returns
@@ -80,11 +80,94 @@ plt.set_xlabel="energy"
 plt.set_ylabel="absorption"
 plt.show()
 
-# s1_energy=s1.Energy.to_numpy()
-# s1_subs_mu=s1['subs mu'].to_numpy()
-# s1_mid=281
-# s1_fac=np.mean(s1_subs_mu[s1_mid-int(midlength/2):s1_mid+int(midlength/2)],dtype=np.float64)
-# s1_norm_mu=s1_subs_mu/s1_fac
 def normalization(df,center,midrange):
-    df_fac=np.mean(df['mu_subs'])
+    """
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        df is the dataframe that is directly read from excel file.
+    center : int
+        it records the center of ranges of data taken for average when normalization
+    midrange : int
+        it records half the number of data taken for average when normalization
+
+    Returns
+    -------
+    df_norm_mu : Pandas DataFrame
+        it is a DataFrame of width 1 that records the data after background substraction
+        and normalization
+
+    """
+    df_fac=np.mean(df['substracted_mu'][center-midrange:center+midrange])
+    df_norm_mu=df['substracted_mu']/df_fac
+    return df_norm_mu
+
+s1["mu_norm"]=normalization(s1,281,25)
+s2["mu_norm"]=normalization(s2,443,50)
+s3["mu_norm"]=normalization(s3, 443, 50)
+s4["mu_norm"]=normalization(s4, 443, 50)
+s5["mu_norm"]=normalization(s5, 443, 50)
+
+def norm_merge(df1,df2):
+    """
     
+
+    Parameters
+    ----------
+    df1 : Pandas DataFrame
+        The "base" dataframe for merging. It is suggested that you take the dataframe
+        with fewer elements as df1 for higher accuracy. df1 should have only
+        one target to merge. (You cannot do a nested method for norm_merge)
+    df2 : Pandas DataFrame
+        df2 is being merged into df1.
+
+    Returns
+    -------
+    merged : Pandas DataFrame
+        based on taking averages, more rows will be added to df2 so that there is 
+        a corresponding value in df2 for every energy in df1. Those rows with energy not 
+        present in df1 will be deleted. merged is the DataFrame after merging 
+        df1 and df2. df2 should have only one target to merge. 
+        (You cannot do a nested method for norm_merge)
+
+    """
+    i=0
+    j=0
+    while((i<df1.shape[0]-1)and(j<df2.shape[0]-1)):
+        while(df1.Energy[i]>df2.Energy[j]):
+            if(not(df2.Energy[j+1])>df1.Energy[i]):
+                j=j+1
+            else:
+                df2_value=(df1.Energy[i]-df2.Energy[j])*(df2.mu_norm[j+1]-df2.mu_norm[j])
+                df2_value=df2_value/(df2.Energy[j+1]-df2.Energy[j])
+                df2_value=df2_value+df2.mu_norm[j]
+                data=pd.DataFrame({'Energy':df1.Energy[i],
+                                   'mu_norm':df2_value},index=[j+0.5])
+                df2=df2.append(data,ignore_index=False)
+                df2=df2.sort_index().reset_index(drop=True)
+                j=j+1
+        i=i+1
+    merged=pd.merge(df1,df2,how="inner",on="Energy")
+    return merged
+result=norm_merge(s1,s2)
+norm=pd.DataFrame({'Energy':s1['Energy'][0:323],"s1_norm_mu":norm_merge(s1,s2[0:614])['mu_norm_x'],
+                   "s2_norm_mu":norm_merge(s1,s2[0:614])['mu_norm_y'],
+                   "s3_norm_mu":norm_merge(s1,s3[0:614])['mu_norm_y'],
+                   "s4_norm_mu":norm_merge(s1,s4[0:614])['mu_norm_y'],
+                   "s5_norm_mu":norm_merge(s1,s5[0:614])['mu_norm_y']})
+
+#plot the superposed normalized merged figures
+plt.plot(norm['Energy'],norm['s1_norm_mu'],label="sasph008")
+plt.plot(norm['Energy'],norm['s2_norm_mu'],label="sdbs034")
+plt.plot(norm['Energy'],norm['s3_norm_mu'],label="sdbso042")
+plt.plot(norm['Energy'],norm['s4_norm_mu'],label="sdbt029")
+plt.plot(norm['Energy'],norm['s5_norm_mu'],label="sfeso4051")
+plt.legend(loc="upper right")
+plt.set_xlabel="energy"
+plt.set_ylabel="absorption"
+plt.show()
+
+
+#export the excel file
+# norm.to_excel(r'C:\Users\lenovo\OneDrive\桌面\norm.xlsx', index = False, header=True)
