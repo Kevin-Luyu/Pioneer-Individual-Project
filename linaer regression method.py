fd5592800751466b3413c55506747c6c521b69ff
Yu Lu <tcsxxs@gmail.com>
2020-08-21 16:15:35 +0800

    File Delete
    delete linear_regression_method.py because it is useless (the one with negative and non-zero intercept and is outcompeted by the scipy one)
    
    delete 未命名0.py because it is created for temporary use. (content is use a function to calculate chi2)

--- linaer regression method.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-norm = pd.read_excel (r'C:\Users\lenovo\OneDrive\桌面\Physics\Pioneer Academics\Independent Project\Data\norm.xlsx')
-s=norm[['s2_norm_mu','s3_norm_mu','s4_norm_mu','s5_norm_mu']]
-s1=norm[['s1_norm_mu']]
-reg = LinearRegression().fit(s, s1)
-print(reg.coef_)
-print(reg.intercept_)
