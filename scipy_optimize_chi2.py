31a06553dd52a0d7e77a262521ec2fb260954546
Yu Lu <tcsxxs@gmail.com>
2020-08-21 16:11:32 +0800

    Update scipy_optimize_chi2.py
    Finish both methods in scipy to minimize chi2. The first is linear-regression to find a starting c0 and then find a minimized c nearby (locally, because it is assumed that these two minimal points are near) 2. use a global minimal method.
    
    Also, the constraint of cs sum to 1 is done by constraint in optimization instead of dividing by thier sum. The later one will cause a inconsistancy in the value of c before and after the finction is called.

--- scipy_optimize_chi2.py
+++ scipy_optimize_chi2.py
@@ -1,6 +1,116 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy import optimize
 norm = pd.read_excel (r'C:\Users\lenovo\OneDrive\桌面\Physics\Pioneer Academics\Independent Project\Data\norm.xlsx')
+known_num=norm.shape[1]-2
 def chisq(c):
+    """
+
+    Parameters
+    ----------
+    c : list
+        c records the coefficients used in the model. c should have a length 
+        equal to known_num
+
+    Returns
+    -------
+    chisq : float
+        model is the predicted behavior of c2*s2+c3*s3+...+cn*sn. s2,s3,...sn are 
+        the known samples presented in norm. chisq records the chi-square 
+        between model and norm['s1_orm_mu'], which is the unknown sample.
+
+    """
+    model=np.zeros(norm.shape[0])
+    for i in range(known_num):
+        name="s"+str(i+2)+"_norm_mu"
+        model=model+c[i]*norm[name].to_numpy()
+    actual=norm['s1_norm_mu'].to_numpy()
+    chisq=np.sum(((actual.astype(np.float64)-model)**2)/(model))
+    return chisq
+
+
+# use scipy.optimize.minimize to optimize the list c, starting from the best result 
+# gained at linear regression
+c0=[0.11,0.05,0.8,0.04]
+bounds=[(0,1),(0,1),(0,1),(0,1)]
+#constraint that the sum of elements in c is 1
+def con(c):
+    """
+    
+
+    Parameters
+    ----------
+    c : list
+        c records the coefficients used in the model. c should have a length 
+        equal to known_num
+
+    Returns
+    -------
+    c_sum-1: float64
+             the difference the sum of all elements in c and 1
+
+    """
+    c_sum=0
+    for i in range (len(c)):
+        c_sum=c_sum+c[i]
+    return (c_sum-1)
+cons = {'type':'eq', 'fun': con}
+res=optimize.minimize(chisq,c0,bounds=bounds,constraints=cons)
+print(res)
+
+#record the best result
+c=res.x
+model=np.zeros(norm.shape[0])
+for i in range(known_num):
+        name="s"+str(i+2)+"_norm_mu"
+        model=model+c[i]*norm[name].to_numpy()
+#plot the figure to compare and print the chisq using scipy.stats to verify
+plt.plot(norm["Energy"],norm["s1_norm_mu"],label="actual")
+plt.plot(norm["Energy"],model,label="predicted")
+plt.legend(loc="upper right")
+plt.set_xlabel="energy"
+plt.set_ylabel="absorption"
+plt.show()
+
+
+
+#use scipy.optimize.shgo to optimize the list c, no initial conditioned needed
+#never use (0,1) because that leads to a fail in global minimalization
+bounds=[(0.001,1),(0.001,1),(0.001,1),(0.001,1)]
+#constraint that the sum of elements in c is 1
+def con(c):
+    """
+    Parameters
+    ----------
+    c : list
+        c records the coefficients used in the model. c should have a length 
+        equal to known_num
+
+    Returns
+    -------
+    c_sum-1: float64
+             the difference the sum of all elements in c and 1
+    """
+    c_sum=0
+    for i in range (len(c)):
+        c_sum=c_sum+c[i]
+    return (c_sum-1)
+cons = {'type':'eq', 'fun': con}
+res=optimize.shgo(chisq,bounds,constraints=cons)
+print(res)
+
+#record the best result
+c=res.x
+model=np.zeros(norm.shape[0])
+for i in range(known_num):
+        name="s"+str(i+2)+"_norm_mu"
+        model=model+c[i]*norm[name].to_numpy()
+#plot the figure to compare and print the chisq using scipy.stats to verify
+plt.plot(norm["Energy"],norm["s1_norm_mu"],label="actual")
+plt.plot(norm["Energy"],model,label="predicted")
+plt.legend(loc="upper right")
+plt.set_xlabel="energy"
+plt.set_ylabel="absorption"
+plt.show()
