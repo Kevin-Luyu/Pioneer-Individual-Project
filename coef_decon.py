611ece971e2b766dbdda48d80f93ca7b4a1f056d
Yu Lu <tcsxxs@gmail.com>
2020-08-30 23:39:20 +0800

    Deconvolution (not a function) of known sample
    perform a deconvolution analysis on s2, wait to see what will happen if I do not limit the center to be plus/minus 1 of initial value. And try to functionalize this and apply it to more unknowns.

--- coef_decon.py
+++ coef_decon.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import optimize
 from scipy import stats
-from lmfit import models
+from lmfit.models import StepModel, LorentzianModel
 norm = pd.read_excel (r'C:\Users\lenovo\OneDrive\桌面\norm.xlsx')
 def inflection(energy,df):
     """
@@ -31,6 +31,7 @@ def inflection(energy,df):
     slope_min=np.amin(-1*slope)
     index=np.where(-slope_min-slope==0)[0]
     return energy[index].item()
+
 def atan_model(x,amplitude,center,length):
     """
 
@@ -72,11 +73,55 @@ def atan_known(energy,df):
     """
     center=inflection(energy,df)
     return atan_model(energy,1.0,center,1.0)
-norm['s2_norm_mu']=norm['s2_norm_mu']-atan_known(norm.Energy,norm['s2_norm_mu'])
+
+"""norm['s2_norm_mu']=norm['s2_norm_mu']-atan_known(norm.Energy,norm['s2_norm_mu'])
 norm['s3_norm_mu']=norm['s3_norm_mu']-atan_known(norm.Energy,norm['s3_norm_mu'])
 norm['s4_norm_mu']=norm['s4_norm_mu']-atan_known(norm.Energy,norm['s4_norm_mu'])
 norm['s5_norm_mu']=norm['s5_norm_mu']-atan_known(norm.Energy,norm['s5_norm_mu'])
 norm['s6_norm_mu']=norm['s6_norm_mu']-atan_known(norm.Energy,norm['s6_norm_mu'])
+"""
 
+#deconvolute for s2
+arctan_mod=StepModel(form='atan',prefix='arctan_')
+paras=arctan_mod.make_params()
+paras['arctan_center'].set(value=inflection(norm.Energy,norm['s2_norm_mu']),vary=False)
+paras['arctan_amplitude'].set(value=1.0,vary=False)
+paras['arctan_sigma'].set(value=1.0)
 
-# norm.to_excel(r'C:\Users\lenovo\OneDrive\桌面\atannorm.xlsx', index = False, header=True)
+lor1=LorentzianModel(prefix='l1_')
+paras.update(lor1.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2472.5))
+#allowed variance of peak position
+length=1.0
+paras['l1_center'].set(value=2472.5,min=2472.5-length,max=2472.5+length)
+
+lor2=LorentzianModel(prefix='l2_')
+paras.update(lor2.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2475.4))
+paras['l2_center'].set(value=2475.4,min=2475.4-length,max=2475.4+length)
+
+lor3=LorentzianModel(prefix='l3_')
+paras.update(lor3.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2478.9))
+
+lor4=LorentzianModel(prefix='l4_')
+paras.update(lor4.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2483.2))
+
+mod=arctan_mod+lor1+lor2+lor3+lor4
+init = mod.eval(paras, x=norm.Energy)
+out=mod.fit(norm['s2_norm_mu'],paras,x=norm.Energy)
+print(out.fit_report())
+
+fig, axes = plt.subplots(1, 2, figsize=(12.8, 4.8))
+axes[0].plot(norm.Energy, norm['s2_norm_mu'], 'b')
+axes[0].plot(norm.Energy, init, 'k--', label='initial fit')
+axes[0].plot(norm.Energy, out.best_fit, 'r-', label='best fit')
+axes[0].legend(loc='best')
+
+comps = out.eval_components(x=norm.Energy)
+axes[1].plot(norm.Energy, norm['s2_norm_mu'], 'b')
+axes[1].plot(norm.Energy, comps['l1_'], label='Lorentzian component 1')
+axes[1].plot(norm.Energy, comps['l2_'], label='Lorentzian component 2')
+axes[1].plot(norm.Energy, comps['l3_'], label='Lorentzian component 3')
+axes[1].plot(norm.Energy, comps['l4_'], label='Lorentzian component 4')
+axes[1].plot(norm.Energy, comps['arctan_'], label='arctangent component')
+axes[1].legend(loc='best')
+
+plt.show()
