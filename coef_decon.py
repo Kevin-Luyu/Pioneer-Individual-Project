1fbe20d3ddb9b2490fd1f3a61d70b3ccc7a74a25
Yu Lu <tcsxxs@gmail.com>
2020-08-31 15:39:22 +0800

    Funcionalize deconvolution of known samples
    use functions to describe the deconvolution of known samples.

--- coef_decon.py
+++ coef_decon.py
@@ -32,96 +32,147 @@ def inflection(energy,df):
     index=np.where(-slope_min-slope==0)[0]
     return energy[index].item()
 
-def atan_model(x,amplitude,center,length):
-    """
-
-    Parameters
-    ----------
-    x : numpy array
-        the energy array.
-    amplitude : float
-        the amplitude of the model.
-    center : float
-        the center of the peak.
-    length : float
-        characteristic widths of the model (half-length of half-peak).
-
-    Returns
-    -------
-    model : numpy array
-        the peak array.
-
-    """
-    model=1*((1/np.pi)*np.arctan((x-center)/length)+1/2)
-    return model
-def atan_known(energy,df):
-    """
-    
-
-    Parameters
-    ----------
-    energy : numpy array
-        the energy array.
-    df : numpy array
-        the array records linear absorption coef of a known sample.
-
-    Returns
-    -------
-    numpy array
-        the arctangent step function associate with a known function.
-
-    """
-    center=inflection(energy,df)
-    return atan_model(energy,1.0,center,1.0)
-
 """norm['s2_norm_mu']=norm['s2_norm_mu']-atan_known(norm.Energy,norm['s2_norm_mu'])
 norm['s3_norm_mu']=norm['s3_norm_mu']-atan_known(norm.Energy,norm['s3_norm_mu'])
 norm['s4_norm_mu']=norm['s4_norm_mu']-atan_known(norm.Energy,norm['s4_norm_mu'])
 norm['s5_norm_mu']=norm['s5_norm_mu']-atan_known(norm.Energy,norm['s5_norm_mu'])
 norm['s6_norm_mu']=norm['s6_norm_mu']-atan_known(norm.Energy,norm['s6_norm_mu'])
 """
-
-#deconvolute for s2
 arctan_mod=StepModel(form='atan',prefix='arctan_')
 paras=arctan_mod.make_params()
+def make_lor(df,num,center,length):
+    """
+    This method do a single-peak Lorentzian deconvolution for a given dataframe
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        df is a column-wise dataframe that records the data you want to 
+        deconvolve.
+    num : int
+        a positional keyword, indicating which index of center array the 
+        Lorentzian in this method is building its center on.
+    center : array
+        the array recording the centers of all peaks.
+    length : float 
+        the maximum allowed variance in the optimized position of peaks from their
+        initila values indicated in the center array.
+
+    Returns
+    -------
+    dict
+        model: the single-peak Lorentzian corresponded.
+        paras: the parameters optimized through this function. This is useful
+        in updating the parameters object paras
+
+    """
+    pref='l'+str(num)+'_'
+    model=LorentzianModel(prefix=pref)
+    paras.update(model.guess(df,x=norm.Energy,center=center[num]))
+    name=pref+'center'
+    paras[name].set(value=center[num],min=center[num]-length,max=center[num]+length)
+    paras[pref+'amplitude'].set(min=0)
+    paras[pref+'sigma'].set(min=0)
+    return {'model':model,'paras':paras}
+def decon_known(df,center):
+    """
+    This method uses multiple make_lor to do a multiple-peak deconvolutoin using 
+    multiple Lorentzians and one arc tangent step function
+
+    Notice: parameters are NOT automatically deleted if they are not used in the 
+    deconvolution of data with fewer peaks than the last time
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        df is a column-wise dataframe that records the data you want to 
+        deconvolve.
+    center : array
+        the array recording the centers of all peaks.
+
+    Returns
+    -------
+    out : lmfit.Model
+        the composit multiple-peak model for the deconvolution.
+
+    """
+    arctan_mod=StepModel(form='atan',prefix='arctan_')
+    paras=arctan_mod.make_params()
+    paras['arctan_center'].set(value=inflection(norm.Energy,df),vary=False,min=0)
+    paras['arctan_amplitude'].set(value=1.0,vary=False)
+    paras['arctan_sigma'].set(value=1.0,min=0)
+    mod=arctan_mod
+    for i in range(len(center)):
+        this=make_lor(df,i,center,2.0)['model']
+        mod=mod+this
+        paras.update(make_lor(df,i,center,2.0)['paras'])
+    out=mod.fit(df,params=paras,x=norm.Energy,weights=df)
+    return out
+
+# print(decon_known(norm['s2_norm_mu'],[2472.5,2475.4,2478.9,2483.2]).fit_report())
+def show_decon_known(df,center):
+    """
+    
+    This method displays the result of deconvolution by
+    1. pring the report of fitting
+    2. draw a figure displaying the best-fit compared with raw data and the 
+    splitting of peaks
+    
+   
+    Parameters
+    ----------
+    df : pandas dataframe
+        df is a column-wise dataframe that records the data you want to 
+        deconvolve.
+    center : array
+        the array recording the centers of all peaks.
+
+    Returns
+    -------
+    None.
+
+    """
+    out=decon_known(df,center)
+    print(out.fit_report())
+    
+    fig, axes = plt.subplots(1, 2, figsize=(12.8, 4.8))
+    axes[0].plot(norm.Energy, df, 'b')
+    axes[0].plot(norm.Energy, out.best_fit, 'r-', label='best fit')
+    axes[0].legend(loc='best')
+    
+    comps = out.eval_components(x=norm.Energy)
+    axes[1].plot(norm.Energy, df, 'b')
+    for i in range (len(center)):
+        name='l'+str(i)+'_'
+        lb='Lorentzian component '+str(i)
+        axes[1].plot(norm.Energy,comps[name],label=lb)
+    axes[1].plot(norm.Energy, comps['arctan_'], label='arctangent component')
+    axes[1].legend(loc='best')
+    
+# deconvolute for s2
 paras['arctan_center'].set(value=inflection(norm.Energy,norm['s2_norm_mu']),vary=False)
 paras['arctan_amplitude'].set(value=1.0,vary=False)
-paras['arctan_sigma'].set(value=1.0)
+paras['arctan_sigma'].set(value=1.0,min=0)
+show_decon_known(norm['s2_norm_mu'],[2472.5,2475.4,2478.9,2483.2])
 
-lor1=LorentzianModel(prefix='l1_')
-paras.update(lor1.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2472.5))
-#allowed variance of peak position
-length=1.0
-paras['l1_center'].set(value=2472.5,min=2472.5-length,max=2472.5+length)
+#deconvolve for s3
+paras['arctan_center'].set(value=inflection(norm.Energy,norm['s3_norm_mu']),vary=False)
+paras['arctan_sigma'].set(value=1.0,min=0)
+show_decon_known(norm['s3_norm_mu'],[2475.26,2478.20,2489.90,2484.52])
 
-lor2=LorentzianModel(prefix='l2_')
-paras.update(lor2.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2475.4))
-paras['l2_center'].set(value=2475.4,min=2475.4-length,max=2475.4+length)
+#deconvolve for s4
+paras['arctan_center'].set(value=inflection(norm.Energy,norm['s4_norm_mu']),vary=False)
+paras['arctan_sigma'].set(value=1.0,min=0)
+show_decon_known(norm['s4_norm_mu'],[2473.05,2476.22,2481.59])
 
-lor3=LorentzianModel(prefix='l3_')
-paras.update(lor3.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2478.9))
+#deconvolve for s5
+paras['arctan_center'].set(value=inflection(norm.Energy,norm['s5_norm_mu']),vary=False)
+paras['arctan_sigma'].set(value=1.0,min=0)
+show_decon_known(norm['s5_norm_mu'],[2481.76,2497.41])
 
-lor4=LorentzianModel(prefix='l4_')
-paras.update(lor4.guess(norm['s2_norm_mu'],x=norm['Energy'],center=2483.2))
-
-mod=arctan_mod+lor1+lor2+lor3+lor4
-init = mod.eval(paras, x=norm.Energy)
-out=mod.fit(norm['s2_norm_mu'],paras,x=norm.Energy)
-print(out.fit_report())
-
-fig, axes = plt.subplots(1, 2, figsize=(12.8, 4.8))
-axes[0].plot(norm.Energy, norm['s2_norm_mu'], 'b')
-axes[0].plot(norm.Energy, init, 'k--', label='initial fit')
-axes[0].plot(norm.Energy, out.best_fit, 'r-', label='best fit')
-axes[0].legend(loc='best')
-
-comps = out.eval_components(x=norm.Energy)
-axes[1].plot(norm.Energy, norm['s2_norm_mu'], 'b')
-axes[1].plot(norm.Energy, comps['l1_'], label='Lorentzian component 1')
-axes[1].plot(norm.Energy, comps['l2_'], label='Lorentzian component 2')
-axes[1].plot(norm.Energy, comps['l3_'], label='Lorentzian component 3')
-axes[1].plot(norm.Energy, comps['l4_'], label='Lorentzian component 4')
-axes[1].plot(norm.Energy, comps['arctan_'], label='arctangent component')
-axes[1].legend(loc='best')
+#deconvolve for s6
+paras['arctan_center'].set(value=inflection(norm.Energy,norm['s6_norm_mu']),vary=False)
+paras['arctan_sigma'].set(value=1.0,min=0)
+show_decon_known(norm['s6_norm_mu'],[2478.79,2484.32,2494.86])
 
 plt.show()
